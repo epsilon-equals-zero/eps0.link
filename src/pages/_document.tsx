a4d6079cd750da54a836424c4767fb1955bd0766
Ian Hornik <yiliansource@gmail.com>
2023-03-28 01:05:21 +0200

    fix open graph tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,15 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                {/* HTML Meta Tags */}
                 <meta
                     name="description"
                     content="A URL shortener for 'ε = 0' related links."
-                    key="desc"
                 />
+
+                {/* Facebook Meta Tags */}
+                <meta property="og:url" content="https://eps0.link/" />
+                <meta property="og:type" content="website" />
                 <meta property="og:title" content="eps0.link" />
                 <meta
                     property="og:description"
@@ -16,6 +20,20 @@ export default function Document() {
                 />
                 <meta
                     property="og:image"
+                    content="https://eps0.link/epsilon-equals-zero.png"
+                />
+
+                {/* Twitter Meta Tags */}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta property="twitter:domain" content="eps0.link" />
+                <meta property="twitter:url" content="https://eps0.link/" />
+                <meta name="twitter:title" content="eps0.link" />
+                <meta
+                    name="twitter:description"
+                    content="A URL shortener for 'ε = 0' related links."
+                />
+                <meta
+                    name="twitter:image"
                     content="https://eps0.link/epsilon-equals-zero.png"
                 />
             </Head>
